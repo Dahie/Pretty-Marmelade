0368b16fe77d7cf36a2c14bc8462e623c820dbda
Daniel Senff <mail@danielsenff.de>
2020-06-30 09:54:30 +0200

    rough display a car with babylon js

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module PrettyMarmelade
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
+    config.assets.precompile += %w[*.png *.bmp *.jpg *.jpeg *.gif *.glb]
+
   end
 end
